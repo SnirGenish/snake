5fea859d48c9134ec793eb863203e0788a9696df
Snork712 <101462694+Snork712@users.noreply.github.com>
2022-04-28 14:30:11 +0300

    Delete js3 directory

--- js3/5.1.js
+++
@@ -1,8 +0,0 @@
-function countryToLiveIn(language, isIsland, population, country) {
-  if (language === "english" && isIsland === false && population < 50000000) {
-    console.log(country + " good");
-  } else {
-    console.log("not good");
-  }
-}
-countryToLiveIn("english", false, 400, "usa");
